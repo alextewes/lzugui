0a19e5110b13cc3d1066bc57fc501c779546bde1
Alexander Tewes <alexandertewes7@gmail.com>
2023-08-14 13:40:03 +0200

    implement status and save-state api

--- src/components/SaveStateComponent.js
+++ src/components/SaveStateComponent.js
@@ -1,24 +1,39 @@
-// SaveStateComponent.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import { useFormik } from 'formik';
-import { StyledForm, StyledLabel, StyledInput, StyledButton } from '../styledComponents';
+import { StyledForm, StyledButton, StyledMessage } from '../styledComponents';
 
 function SaveStateComponent() {
+    const [message, setMessage] = useState(null);
+
     const formik = useFormik({
-        initialValues: {
-            apiUrl: '',
-        },
-        onSubmit: values => {
-            alert(JSON.stringify(values, null, 2));
+        initialValues: {},
+        onSubmit: () => {
+            fetch('http://localhost:8080/api/components/save-state', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.text();
+                })
+                .then(data => {
+                    setMessage(data);
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                    setMessage('Error saving state');
+                });
         },
     });
 
     return (
         <StyledForm onSubmit={formik.handleSubmit}>
-            <StyledLabel htmlFor="apiUrl">API URL</StyledLabel>
-            <StyledInput id="apiUrl" name="apiUrl" type="text" onChange={formik.handleChange} value={formik.values.apiUrl} />
-
             <StyledButton type="submit">Save State</StyledButton>
+            {message && <StyledMessage>{message}</StyledMessage>}
         </StyledForm>
     );
 }
--- src/components/StatusComponent.js
+++ src/components/StatusComponent.js
@@ -1,15 +1,25 @@
-// StatusComponent.jsx
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyledTable, StyledTableHeader, StyledTableData, StyledTableRow } from '../styledComponents';
 
-// Mocked data
-const statuses = [
-    { id: 1, name: 'Component 1', status: 'running' },
-    { id: 2, name: 'Component 2', status: 'stopped' },
-    { id: 3, name: 'Component 3', status: 'running' },
-];
-
 function StatusComponent() {
+    const [statuses, setStatuses] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/components/status')
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setStatuses(data);
+            })
+            .catch((error) => {
+                console.error('There has been a problem with your fetch operation:', error);
+            });
+    }, []);
+
     return (
         <StyledTable>
             <thead>
@@ -20,13 +30,14 @@ function StatusComponent() {
             </StyledTableRow>
             </thead>
             <tbody>
-            {statuses.map((status) => (
-                <StyledTableRow key={status.id}>
-                    <StyledTableData>{status.id}</StyledTableData>
+            {statuses.map((status, index) => (
+                <StyledTableRow key={index}>
+                    <StyledTableData>{status.ID}</StyledTableData>
                     <StyledTableData>{status.name}</StyledTableData>
-                    <StyledTableData>{status.status}</StyledTableData>
+                    <StyledTableData>{status.state}</StyledTableData>
                 </StyledTableRow>
             ))}
+
             </tbody>
         </StyledTable>
     );
